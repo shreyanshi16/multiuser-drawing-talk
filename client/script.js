31a2739090b42bdfb70365b13d84e51e09e84113
shreyanshi16 <53462278+shreyanshi16@users.noreply.github.com>
2021-01-10 15:29:44 +0530

    Initial commit

---
+++ client/script.js
@@ -0,0 +1,32 @@
+//This is jquery syantax to autoexecute function once
+let socket, sendMessageBoard;
+$(() => {
+  socket = io.connect("http://localhost:8000");
+  var sendMessageBoard = new DrawingBoard.Board('sendMessageBoard', {
+    background: "#ff7ffe",
+  	color: "#fff",
+  	size: 30,
+  	controls: [
+  		{ Size: { type: "range" } },
+  		{ Navigation: { back: false, forward: false } },
+  		'DrawingMode'
+  	],
+  	webStorage: 'local'
+});
+  $("#sendDWGbtn").click(() => {
+    // Send image to server on button click
+    //console.log("SEND DRAWINGG");
+    socket.emit("drawing", sendMessageBoard.getImg());
+    //After sending image clears out the canvas
+    sendMessageBoard.resetBackground();
+    return false;
+  });
+  socket.on("drawing", function (msg) {
+    $("#messageContainer").append(
+      $("<li class='w-100 d-flex align-center justify-content-center'>").html(
+        `<img src="${msg}" class="w-75 m-auto img-msg"/>`
+      )
+    );
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+});
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var PORT = process.env.PORT || 8000;
+
+app.use(express.static(`${__dirname}/client`));
+
+// app.get("/", (req, res) => {
+//   console.log("Get request to Homepage");
+//   res.send("Hiii sent by server...");
+// });
+
+//Realtime message sending socket part
+io.on("connection", function (socket) {
+  //On getting drawing from request emit it to all users
+  socket.on("drawing", function (dwg) {
+    io.emit("drawing", dwg);
+  });
+});
+
+http.listen(PORT, function () {
+  console.log(`Server Started on PORT: ${PORT}`);
+});
